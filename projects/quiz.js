039dee69bd3645b138fcf9c68a720727c676494d
tpauldike <topman4loveworld@gmail.com>
2023-05-11 14:00:25 +0000

    changed the structure a bit

--- projects/quiz.js
+++ projects/quiz.js
@@ -1,7 +1,22 @@
 const quiz = {
 	'Q1': {
 		'question':'Who is the founder of CodeTrybe?',
-		'options': {
-			'a': 'a. Abiodun Shittu',
-			'b': 'b. Topman Paul-Dike,
-			'c': '	
+		'options': [
+			'a. Abiodun Shittu',
+			'b. Topman Paul-Dike',
+			'c. Ebube Anyanwu',
+			'd. Naheemah Bello'
+		]
+	},
+	'Q2': {
+		'question':
+}
+
+//How the question should print:
+/*
+console.log(quiz.Q1.question);
+for (let i = 0; i < 4; i++){
+	console.log(quiz.Q1.options[i]);
+}
+*/
+
