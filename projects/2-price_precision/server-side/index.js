e3dfe271df8720c5b88fa535eff37cdc0d7a61db
Abiodun Shittu <jerrywizklay1@gmail.com>
2023-07-03 15:00:20 -0400

    Feat: Add product Endpoints

--- projects/2-price_precision/server-side/index.js
+++ projects/2-price_precision/server-side/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connection from "./src/database/db.js";
 import userRouter from "./src/routes/user.route.js"
+import productRouter from "./src/routes/product.route.js"
 dotenv.config();
 
 const app  = express();
@@ -17,7 +18,8 @@ connection;
 
 // mount the  router
 app.use("/user", userRouter);
+app.use("/products", productRouter);
 
 app.listen(port, () => {
 	console.log(`This server is listening on http://localhost:${port}`);
-})
+})	
--- projects/2-price_precision/server-side/src/routes/user.route.js
+++ projects/2-price_precision/server-side/src/routes/user.route.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { createUser } from '../controllers/user.controller.js';
+import { createUser, loginUser } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 router.post("/create", createUser) // create a new user
-// router.post("/login", handler) // login the user
+router.post("/login", loginUser) // login the user
 
 export default router;
