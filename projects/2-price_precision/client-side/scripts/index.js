1db879cab7eab5ed54e28465a0d9a8284de49a6b
tpauldike <topman4loveworld@gmail.com>
2023-07-20 21:10:53 +0000

    implemented the landing page in index.html, index.css and index.js

--- projects/2-price_precision/client-side/scripts/index.js
+++ projects/2-price_precision/client-side/scripts/index.js
@@ -1,1 +1,22 @@
-    
+const signupButton = document.getElementById('signup-button');
+const signinButton = document.getElementById('signin-button');
+const signupFormContainer = document.getElementById('signup-form-container');
+const signinFormContainer = document.getElementById('signin-form-container');
+
+signupButton.addEventListener('click', () => {
+  signupFormContainer.classList.add('is-visible');
+});
+
+signinButton.addEventListener('click', () => {
+  signinFormContainer.classList.add('is-visible');
+});
+
+document.addEventListener('click', (event) => {
+  if (!signupFormContainer.contains(event.target) && !event.target.matches('#signup-button')) {
+    signupFormContainer.classList.remove('is-visible');
+  }
+  
+  if (!signinFormContainer.contains(event.target) && !event.target.matches('#signin-button')) {
+    signinFormContainer.classList.remove('is-visible');
+  }
+});
