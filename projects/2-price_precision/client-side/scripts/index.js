7ab42d5a07edc05a340484f834e0fafb2e9a2b73
Abiodun Shittu <jerrywizklay1@gmail.com>
2023-07-31 05:38:12 -0400

    Api Consumption for Signup and Login

--- projects/2-price_precision/client-side/scripts/index.js
+++ projects/2-price_precision/client-side/scripts/index.js
@@ -11,12 +11,109 @@ signinButton.addEventListener('click', () => {
   signinFormContainer.classList.add('is-visible');
 });
 
-document.addEventListener('click', (event) => {
-  if (!signupFormContainer.contains(event.target) && !event.target.matches('#signup-button')) {
-    signupFormContainer.classList.remove('is-visible');
-  }
+// document.addEventListener('click', (event) => {
+//   if (!signupFormContainer.contains(event.target) && !event.target.matches('#signup-button')) {
+//     signupFormContainer.classList.remove('is-visible');
+//   }
   
-  if (!signinFormContainer.contains(event.target) && !event.target.matches('#signin-button')) {
-    signinFormContainer.classList.remove('is-visible');
+//   if (!signinFormContainer.contains(event.target) && !event.target.matches('#signin-button')) {
+//     signinFormContainer.classList.remove('is-visible');
+//   }
+// });
+
+const signUpForm = document.getElementById("signup-form");
+const signInForm = document.getElementById("signin-form");
+const baseUrl = "http://localhost:4001";
+
+signUpForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  // console.log("Hello World!");
+
+  const registerButton = document.getElementById("register");
+  registerButton.innerHTML = "Loading...";
+
+  // Get the form data
+  const firstNameInput = document.getElementById("firstname")
+  const lastNameInput = document.getElementById("lastname")
+  const genderInput = document.getElementById("gender")
+  const phoneNumberInput = document.getElementById("phone")
+  const emailInput = document.getElementById("email")
+  const passwordInput = document.getElementById("password")
+  const businessNameInput = document.getElementById("businessName")
+  const descriptionInput = document.getElementById("description")
+  const businessAddressInput = document.getElementById("businessAddress")
+
+  const formData = {
+    first_name: firstNameInput.value,
+    last_name: lastNameInput.value,
+    gender: genderInput.value,
+    phone: phoneNumberInput.value,
+    email: emailInput.value,
+    password: passwordInput.value,
+    business_name: businessNameInput.value,
+    description: descriptionInput.value,
+    business_address: businessAddressInput.value
   }
-});
+
+  // console.log(formData);
+  try {
+    // Make the fetch request to the Backend API to register user
+    const response = await fetch(`${baseUrl}/user/create`, {
+      method: 'POST',
+      headers: { "Content-Type": "application/json"},
+      body: JSON.stringify(formData)
+    })
+
+    const data = await response.json();
+    console.log(data);
+    if (data.message === "User created successfully") {
+      setTimeout(() => {
+        window.location = "./products.html";
+      }, 2000)
+    } else {
+      alert(data.message)
+      registerButton.innerHTML = "Submit"
+    }
+    
+  } catch (error) {
+    console.log("Error ==>", error);
+  }
+
+})
+
+signInForm.addEventListener("submit", async(e) => {
+  e.preventDefault();
+  const loginButton = document.getElementById("login");
+  loginButton.innerHTML = "Loading..."
+
+  // Get the form data
+  const loginEmailInput = document.getElementById("login_email");
+  const loginPasswordInput = document.getElementById("login_password");
+
+  const formData = {
+    email: loginEmailInput.value,
+    password: loginPasswordInput.value
+  }
+
+  try {
+		// Make the fetch request to the Backend API to register user
+		const response = await fetch(`${baseUrl}/user/login`, {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(formData),
+		});
+
+		const data = await response.json();
+		console.log(data);
+		if (data.message === "Login Successful") {
+			setTimeout(() => {
+				window.location = "./products.html";
+			}, 2000);
+		} else {
+			alert(data.message);
+			loginButton.innerHTML = "Submit";
+		}
+  } catch (error) {
+    console.log("Error ==>", error);
+  }
+})
--- projects/2-price_precision/server-side/index.js
+++ projects/2-price_precision/server-side/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors"
 import connection from "./src/database/db.js";
 import userRouter from "./src/routes/user.route.js"
 import productRouter from "./src/routes/product.route.js"
@@ -8,6 +9,7 @@ dotenv.config();
 const app  = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors())
 app.use(express.json());
 
 app.get("/", (req, res) => {
